ebd763819a9311a543f31ef06142d0e0234bef3f
merugukeerthana577-rgb <merugukeerthana577@gmail.com>
2026-07-28 17:01:58 +0530

    implemented rest template

---
+++ src/main/java/com/klef/soa/dto/ProductDTO.java
@@ -0,0 +1,43 @@
+package com.klef.soa.dto;
+
+public class ProductDTO 
+{
+   private int id;
+   private String title;
+   private double price;
+   private String description;
+   private String Category;
+   public int getId() {
+	return id;
+   }
+   public void setId(int id) {
+	this.id = id;
+   }
+   public String getTitle() {
+	return title;
+   }
+   public void setTitle(String title) {
+	this.title = title;
+   }
+   public double getPrice() {
+	return price;
+   }
+   public void setPrice(double price) {
+	this.price = price;
+   }
+   public String getDescription() {
+	return description;
+   }
+   public void setDescription(String description) {
+	this.description = description;
+   }
+   public String getCategory() {
+	return Category;
+   }
+   public void setCategory(String category) {
+	this.Category = category;
+   }
+   
+   }
+  
+
